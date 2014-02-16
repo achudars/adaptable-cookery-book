9b03a191e14e110c33c7d038f4633c34d79be13a
Aleks <achudars@gmail.com>
2014-02-16 18:33:56 +0000

    Added a custom loader to avoid adding header and footer. Easier templating and code reuse.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Team 14 - IAPT Group Assessment</title>
 	</head>
 	<body>
 		<header>
-			<h1>This is a header</h1>
+			<a href="">Home</a>
 		</header>
---
+++ application/views/templates/navigation.php
@@ -0,0 +1,5 @@
+<?php
+
+$this->load->view('templates/header');
+$this->load->view('templates/'.$v);
+$this->load->view('templates/footer');
