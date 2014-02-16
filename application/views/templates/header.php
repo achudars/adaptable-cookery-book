beadab463dc33a910f313783566038a1f06c6ac5
Aleks <achudars@gmail.com>
2014-02-16 22:54:12 +0000

    Added and separated grid and list view.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -30,7 +30,8 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="recipes">Recipes</a></li>
+                        <li class="active"><a href="recipes">GRID Recipes</a></li>
+                        <li><a href="recipes2">LIST Recipes</a></li>
                         <li><a href="recipe">Recipe</a></li>
                     </ul>
                     <form class="navbar-form navbar-right btn-group" role="search">
