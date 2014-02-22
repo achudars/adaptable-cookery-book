354a23339ffe3f5f1508859d6c3328316ee68e43
shnuggie-dan <daniel__knight@talktalk.net>
2014-02-22 00:50:07 +0000

    Added functions to controller to show both the list view for recipes and grid view

--- application/controllers/recipes.php
+++ application/controllers/recipes.php
@@ -5,4 +5,31 @@
 	 */
 	class Recipes extends CI_Controller
 	{
+		/**
+		 * Funciton used to display the recipes in the
+		 * system in their list form.
+		 */
+		public function viewList()
+		{
+			$data['title'] = 'View All Recipes: List View';
+
+			$this->load->helper('html');
+			$this->load->view('templates/header.php', $data);
+			$this->load->view('pages/recipes_list.php', $data);
+			$this->load->view('templates/footer.php', $data);
+		}
+
+		/**
+		 * Function used to display the recipes in the
+		 * system in their grid form.
+		 */
+		public function viewGrid()
+		{
+			$data['title'] = 'View All Recipes: Grid View';
+
+			$this->load->helper('html');
+			$this->load->view('templates/header.php', $data);
+			$this->load->view('pages/recipes_grid.php', $data);
+			$this->load->view('templates/footer.php', $data);
+		}
 	}
