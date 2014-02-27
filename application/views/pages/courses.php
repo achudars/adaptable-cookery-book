739aeb7a28506517e91ac59764d6f1ae46c6aa82
shnuggie-dan <daniel__knight@talktalk.net>
2014-02-27 23:36:46 +0000

    Added HTML to courses page

--- application/views/pages/courses.php
+++ application/views/pages/courses.php
@@ -1,6 +1,20 @@
 <div class="container">
-    <h1>Courses</h1>
-    <div class="row">
-		<p>Here's some filler text that will have courses in it.</p>
-    </div>
+    <?php foreach ($courses as $course):?>
+      <div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
+          <a href="/course">
+            <div class="preview-img" style="background-image: url(<?php echo $course[5];?>);"></div>
+            <div class="course-title">
+                <p> <?php echo $course[0];?> </p>
+            </div>
+            <div class="split">
+                <span><span class="glyphicon glyphicon-time"></span> <?php echo $course[1];?> min</span>
+                <span><?php echo $course[2];?> cal <span class="glyphicon glyphicon-dashboard"></span></span>
+            </div>
+            <div class="split">
+                <span><span class="glyphicon glyphicon-cutlery"></span> <?php echo $course[3];?></span>
+                <span><?php echo $course[4];?> <span class="glyphicon glyphicon-user"></span></span>
+            </div>
+          </a>
+      </div>
+    <?php endforeach;?>
 </div>
