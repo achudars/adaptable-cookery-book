c5c34aafc4f127be24f582e872a35d24922761f5
shnuggie-dan <daniel__knight@talktalk.net>
2014-02-27 23:49:01 +0000

    Added content to the course controller

--- application/controllers/courses.php
+++ application/controllers/courses.php
@@ -12,6 +12,33 @@
 		{
 			$data['title'] = 'Courses Available';
 
+			$data['courses'] = array(
+				array(
+					'courseName'  => 'Starters',
+					'courseId'    => 1,
+					'recipeCount' => 10,
+					'imageUrl'    => 'http://www.bishopburton.ac.uk/_inc/uploads/generalimages/Starters.jpg',
+				),
+				array(
+					'courseName'  => 'Main Courses',
+					'courseId'    => 2,
+					'recipeCount' => 7,
+					'imageUrl'    => 'http://www.brake.co.uk/_assets/720xn/Main_Course_Poster_720x430px_1.jpg',
+				),
+				array(
+					'courseName'  => 'Desserts',
+					'courseId'    => 3,
+					'recipeCount' => 11,
+					'imageUrl'    => 'http://upload.wikimedia.org/wikipedia/commons/8/84/Chocolate_Cake_Flourless_%281%29.jpg',
+				),
+				array(
+					'courseName'  => 'Snacks',
+					'courseId'    => 4,
+					'recipeCount' => 4,
+					'imageUrl'    => 'http://pururuwang.files.wordpress.com/2012/11/snacks1.jpg',
+				)
+			);
+
 			$this->load->helper('html');
 			$this->load->view('templates/header.php', $data);
 			$this->load->view('pages/courses.php', $data);
--- application/views/pages/courses.php
+++ application/views/pages/courses.php
@@ -1,18 +1,13 @@
 <div class="container">
     <?php foreach ($courses as $course):?>
       <div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
-          <a href="/course">
-            <div class="preview-img" style="background-image: url(<?php echo $course[5];?>);"></div>
+          <a href="/course/<?php echo $course['courseId'] ?>">
+            <div class="preview-img" style="background-image: url(<?php echo $course['imageUrl'];?>);"></div>
             <div class="course-title">
-                <p> <?php echo $course[0];?> </p>
+                <p> <?php echo $course['courseName'];?> </p>
             </div>
             <div class="split">
-                <span><span class="glyphicon glyphicon-time"></span> <?php echo $course[1];?> min</span>
-                <span><?php echo $course[2];?> cal <span class="glyphicon glyphicon-dashboard"></span></span>
-            </div>
-            <div class="split">
-                <span><span class="glyphicon glyphicon-cutlery"></span> <?php echo $course[3];?></span>
-                <span><?php echo $course[4];?> <span class="glyphicon glyphicon-user"></span></span>
+                <span><?php echo $course['recipeCount'];?> recipes <span class="glyphicon glyphicon-list"></span></span>
             </div>
           </a>
       </div>
