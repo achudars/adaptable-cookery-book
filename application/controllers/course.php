ac09a822fb06b3b1908b077ea1deb0b55d7999db
shnuggie-dan <daniel__knight@talktalk.net>
2014-02-28 00:11:30 +0000

    Added code to show a grid view of recipes that belong to a type of course. All dummy data.

--- application/controllers/course.php
+++ application/controllers/course.php
@@ -24,6 +24,22 @@
 			$data['courseName']  = 'Main Courses';
 			$data['courseImage'] = 'http://www.brake.co.uk/_assets/720xn/Main_Course_Poster_720x430px_1.jpg';
 
+			$data['recipes'] = array(
+				array("Home Brew","31","100","main dish","1",                   "http://bit.ly/1fkKR4y"),
+				array("Doggy Cake","74","240","main course","2",                  "http://bit.ly/1jT90mm"),
+				array("California pizza","97","130","main dish","3",            "http://bit.ly/1nRcRxJ"),
+				array("Lasagna","89","110","main dish","4",                     "http://bit.ly/1ffWb3r"),
+				array("Mushroom Pie","44","240","main course","3",                "http://bit.ly/1h1JhVd"),
+				array("Extreme Cajun Flamethrower","73","170","main dish","2",  "http://bit.ly/Nf360R"),
+				array("Thai Sweet Crispy Rice Cakes","16","150","main dish","1","http://bit.ly/1c5nWpB"),
+				array("Puffed Rice Cake","26","160","main course","2",            "http://bit.ly/1hbxzpJ"),
+				array("Tapenade","28","220","main course","4",                    "http://bit.ly/1hbxen4"),
+				array("Drummond's Big Sausage","74","290","main dish","5",      "http://bit.ly/1bqoco0"),
+				array("Prawn and Cauliflower Curry","50","340","main dish","23", "http://bit.ly/Nf3mwE"),
+				array("Singapore noodles","0","1","main course","4",              "http://bit.ly/1bqog7m"),
+				array("Singarich noodles","10","100","main dish","0",           "http://bit.ly/1hbxPVG")
+			);
+
 			$this->load->helper('html');
 			$this->load->view('templates/header.php', $data);
 			$this->load->view('pages/course.php', $data);
--- application/views/pages/course.php
+++ application/views/pages/course.php
@@ -6,3 +6,23 @@
 		</div>
 	</div>
 </div>
+<div class="container">
+    <?php foreach ($recipes as $recipe):?>
+      <div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
+          <a href="/recipe">
+            <div class="preview-img" style="background-image: url(<?php echo $recipe[5];?>);"></div>
+            <div class="recipe-title">
+                <p> <?php echo $recipe[0];?> </p>
+            </div>
+            <div class="split">
+                <span><span class="glyphicon glyphicon-time"></span> <?php echo $recipe[1];?> min</span>
+                <span><?php echo $recipe[2];?> cal <span class="glyphicon glyphicon-dashboard"></span></span>
+            </div>
+            <div class="split">
+                <span><span class="glyphicon glyphicon-cutlery"></span> <?php echo $recipe[3];?></span>
+                <span><?php echo $recipe[4];?> <span class="glyphicon glyphicon-user"></span></span>
+            </div>
+          </a>
+      </div>
+    <?php endforeach;?>
+</div>
