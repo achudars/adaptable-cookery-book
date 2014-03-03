9c496997babb686f4ccd4884f634b60b15f7d52c
Aleks <achudars@gmail.com>
2014-03-03 14:03:39 +0000

    Slightly improved getting synonyms.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 (function() {
+    $(".the").tooltip();
 
-  $(".the").hover(function() {
-    var word = $(this).text();
-    find_synonyms(word);
-});
+    $(".the").hover(function() {
+        var word = $(this).text();
+        find_synonyms(word);
+    });
 
 })();
 
 function find_synonyms(word) {
 
     var API_KEY = "3691e6b302f58d4e90d209e0762b1e04";
-    var synonyms = [];
 
     $.ajax({
         type: "GET",
@@ -20,15 +20,12 @@ function find_synonyms(word) {
         dataType: "json",
         success:
         function(data) {
-            synonyms = JSON.parse(JSON.stringify(data.noun.syn));
-            $(".the").attr('title',               synonyms[0]+", "+synonyms[1]+", "+synonyms[2]);
-            $(".the").attr('data-original-title', synonyms[0]+", "+synonyms[1]+", "+synonyms[2]);
-            $(".the").on( "mouseenter", function() {
-                $(this).tooltip('show');
-            })
-            .on( "mouseleave", function() {
-                $(this).tooltip('hide');
+           synonyms = JSON.parse(JSON.stringify(data.noun.syn));
+            $(".the").one().mouseenter(function() {
+                $(this).one().attr('data-original-title', synonyms[0]+", "+synonyms[1]+", "+synonyms[2]);
             });
         }
+    }).done(function(data) {
+
     });
 };
