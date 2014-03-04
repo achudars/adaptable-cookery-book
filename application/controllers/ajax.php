d5af316bb0ec192de17a952bbbd5a6315cdb3b77
shnuggie-dan <daniel__knight@talktalk.net>
2014-03-04 16:19:40 +0000

    Added new controller file

---
+++ application/controllers/ajax.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	 * Controller used to route ajax requests for the application
+	 */
+	class Ajax extends CI_Controller
+	{
+		public function changeRecipeStyle()
+		{
+			error_log("fndsjkndsfnkdsjnfnksdkjnf");
+		}
+	}
