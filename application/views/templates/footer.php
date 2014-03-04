0529260e31ba4693c6f07e30215cbdab5bfd582d
shnuggie-dan <daniel__knight@talktalk.net>
2014-03-04 15:54:08 +0000

    Changed JS include to use the base_url function

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,6 +2,6 @@
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<script src="../../../assets/js/script.js"></script>
+<script src="<?php echo base_url() ?>assets/js/script.js"></script>
 </body>
 </html>
