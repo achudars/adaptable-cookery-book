26f35073e79651c00fa6e43f66ddd439763950cf
shnuggie-dan <daniel__knight@talktalk.net>
2014-03-08 23:39:07 +0000

    Added code to breadcrumb generating function

--- application/models/breadcrumb_model.php
+++ application/models/breadcrumb_model.php
@@ -1,5 +1,9 @@
 <?php
 
+	/**
+	 * Model used to handle creation of the site's
+	 * breadcrumb navigation. Heirachy, not history.
+	 */
 	class Breadcrumb_Model extends CI_Model
 	{
 		public function __construct()
@@ -7,8 +11,29 @@
 			parent::__construct();
 		}
 
-		public function generateFunction($siteSection)
+		public function generateBreadcrumb($siteSection)
 		{
+			if(empty($siteSection))
+			{
+				error_log(__FILE__ . ':' . __LINE__ . ' - Received empty site section. Breadcrumb not generating.');
+				return;
+			}
 
+			$links = array(
+				'Home' => base_url() . 'recipe/grid-view',
+			);
+
+			switch(strtolower($siteSection))
+			{
+				case 'recipe':
+					$links['Recipes'] = base_url() . 'recipe/grid-view';
+					break;
+
+				case 'course':
+					$links['Courses'] = base_url() . 'courses/';
+					break;
+			}
+
+			return $links;
 		}
 	}
