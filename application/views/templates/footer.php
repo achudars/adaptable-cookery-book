cd3dca88968d58d78cca88a43cbf462fdb5690f3
Aleks <achudars@gmail.com>
2014-03-08 21:10:20 +0000

    Added fancy visual pairing of ingredient for segmented and step-by-step views.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,6 @@
 
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/lettering.js/0.6.1/jquery.lettering.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url() ?>assets/js/script.js"></script>
