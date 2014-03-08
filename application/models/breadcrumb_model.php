09c2ecb023e85fdb2642a9b96f9f0e2b3349e385
shnuggie-dan <daniel__knight@talktalk.net>
2014-03-08 22:42:44 +0000

    Added new model for handling breadcrumb generation

---
+++ application/models/breadcrumb_model.php
@@ -0,0 +1,14 @@
+<?php
+
+	class Breadcrumb_Model extends CI_Model
+	{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function generateFunction($siteSection)
+		{
+
+		}
+	}
