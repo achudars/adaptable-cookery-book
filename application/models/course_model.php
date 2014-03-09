5eb95e4142990008ca5cabb706ab5ae4e7be4770
shnuggie-dan <daniel__knight@talktalk.net>
2014-03-09 16:38:28 +0000

    Changed model function to retrieve course detail to use name rather than ID

--- application/models/course_model.php
+++ application/models/course_model.php
@@ -6,35 +6,35 @@ class Course_model extends CI_Model
     {
         parent::__construct();
     }
-    
+
     /**
      * Loads a list of all the types of courses, and their basic information
-     * 
+     *
      * @return Array[Object] [{courseid, name, description, recipes}, ...]
      */
     public function getAllCourses() {
         $this->db->select('courseid, name, description')
                 ->from('course')
                 ->order_by('name', 'asc');
-        
+
         return $this->db->get()->result();
     }
-    
+
     /**
      * Loads information about a course
-     * 
-     * @param int $courseid
+     *
+     * @param String $courseName
      * @return Object {courseid, name, description}
      * @throws Exception if a course does not exist
      */
-    public function getCourseInfo($courseid) {
+    public function getCourseInfo($courseName) {
         $this->db->select('name, description')
                 ->from('course')
-                ->where(['courseid' => $courseid])
+                ->where(['name' => $courseName])
                 ->limit(1);
-        
+
         $q = $this->db->get();
-        
+
         if (empty($q))
         {
             throw new Exception('That course could not be found.', 404);
