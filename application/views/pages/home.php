4a1c0766e75a9246ca34568b7c11a61248bfce3d
Daniel Knight <daniel__knight@talktalk.net>
2014-03-10 16:21:05 +0000

    Added three boxes for styles to the homepage

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,4 +3,15 @@
 		<h1>Home</h1>
 		<p>This is the home page</p>
 	</div>
+	<div class="row-fluid">
+		<div class="span4">
+			<h1>Narrative View</h1>
+		</div>
+		<div class="span4">
+			<h1>Segmented View</h1>
+		</div>
+		<div class="span4">
+			<h1>Step By Step View</h1>
+		</div>
+	</div>
 </div>
