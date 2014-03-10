2ebc8581ecca977261bbf62f7761aba18faf35c9
shnuggie-dan <daniel__knight@talktalk.net>
2014-03-10 21:49:24 +0000

    Added content and images to the home page to explain what each of the recipe styles mean

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,17 +1,47 @@
-<div class="container">
+<div class="container home-intro">
 	<div class="hero-unit">
 		<h1>Home</h1>
 		<p>This is the home page</p>
 	</div>
-	<div class="row-fluid">
-		<div class="span4">
-			<h1>Narrative View</h1>
+</div>
+<div class="container">
+	<div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
+		<div class="holder">
+			<img alt="Image of Segmented Instructions" src="" class="preview-img" style="background-image: url(<?php echo base_url() . 'assets/images/narrative.png' ?>);">
+			<div class="course-title">
+				<p>Narrative Instructions</p>
+			</div>
+			<div class="style-intro">
+				<span>The narrative style is for those who don't want to be bounded by culineray rules. Rough steps, rough instructions.</span>
+				<br />
+				<a class="btn btn-primary recipe-style" data-style="narrative">Choose Narrative Style</a>
+			</div>
 		</div>
-		<div class="span4">
-			<h1>Segmented View</h1>
+	</div>
+	<div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
+		<div class="holder">
+			<img alt="Image of Segmented Instructions" src="" class="preview-img" style="background-image: url(<?php echo base_url() . 'assets/images/segmented.png' ?>);">
+			<div class="course-title">
+				<p>Segmented Instructions</p>
+			</div>
+			<div class="style-intro">
+				<span>The segmented style is well suited to those with intermediate experience in the kitchen, providing the information for the recipe in several, not too complicated steps.</span>
+				<br />
+				<a class="btn btn-primary recipe-style" data-style="segmented">Choose Segmented Style</a>
+			</div>
 		</div>
-		<div class="span4">
-			<h1>Step By Step View</h1>
+	</div>
+	<div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
+		<div class="holder">
+			<img alt="Image of Segmented Instructions" src="" class="preview-img" style="background-image: url(<?php echo base_url() . 'assets/images/steps.png' ?>);">
+			<div class="course-title">
+				<p>Step-By-Step Instructions</p>
+			</div>
+			<div class="style-intro">
+				<span>Not really one for cooking? Not really sure how to sauté a potato? Simple, easy and well defined steps - this is the choice for you.</span>
+				<br />
+				<a class="btn btn-primary recipe-style" data-style="step">Choose Step-By-Step Style</a>
+			</div>
 		</div>
 	</div>
 </div>
