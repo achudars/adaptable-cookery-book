6635ca337fda17e67aa8ce8506cc5863b8f900ba
Daniel Knight <daniel__knight@talktalk.net>
2014-03-10 16:13:16 +0000

    Added data array to home controller, including the preferred style data

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -17,6 +17,23 @@
 		 */
 		public function view()
 		{
+			$data = array(
+				'title' => 'Welcome to the Cookery Application',
+			);
+
+			$this->load->model('Recipe_style_model');
+
+			$preferredRecipeStyle = $this->Recipe_style_model->getRecipeStyle();
+
+			if(!$preferredRecipeStyle)
+			{
+			    $this->Recipe_style_model->setRecipeStyle();
+			    $preferredRecipeStyle = 'narrative';
+			}
+
+			//Go and get the recipe details from the database
+			$data['defaultStyle'] = $preferredRecipeStyle;
+
 			$this->load->helper('html');
 			$this->load->view('templates/header.php', $data);
 			$this->load->view('pages/home.php', $data);
