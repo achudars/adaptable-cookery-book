ae08453b04141358a7522d9d4748da7fa5287a40
Aleks <achudars@gmail.com>
2014-03-10 23:54:40 +0000

    Fixed course text.

--- application/views/pages/courses.php
+++ application/views/pages/courses.php
@@ -1,15 +1,15 @@
 <div class="container">
     <?php foreach ($courses as $course):?>
-      <div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
-          <div class="holder">
+    <div class="grid-recipes col-xs-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="holder">
             <img alt="Image of <?php echo $course->name;?>" src="" class="preview-img" style="background-image: url(http://placehold.it/3500x1500);">
             <div class="course-title">
                 <p><a href="<?php echo base_url() . 'course/' . strtolower($course->name) ?>"><span class="glyphicon glyphicon-chevron-right"></span> <?php echo $course->name;?></a></p>
             </div>
-			<div>
-				<p><?php echo $course->description ?></p>
-			</div>
-          </div>
-      </div>
+            <span class="course-description">
+                <p><?php echo $course->description ?></p>
+            </span>
+        </div>
+    </div>
     <?php endforeach;?>
 </div>
