c26eaa308ea099f27b41e18a4fa589490ec13617
Aleks <achudars@gmail.com>
2014-03-12 17:17:07 +0000

    Fixed meta tag issue raised by W3C validator.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,8 +4,7 @@ header('Content-Type:text/html; charset=utf-8');
 <html lang="en">
 
 <head>
-    <meta content-type="text/html; charset=utf-8">
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title ?></title>
     <link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/favicon.ico" type="image/x-icon">
