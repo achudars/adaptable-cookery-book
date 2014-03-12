8a158e56d983183ce65eb0ba76ed8453c57663a8
Aleks <achudars@gmail.com>
2014-03-12 00:29:30 +0000

    Fixed logica lordering for a single recipe.

--- application/views/pages/recipe.php
+++ application/views/pages/recipe.php
@@ -1,14 +1,14 @@
 <div class="container single-recipe">
     <div class="row">
         <div class="recipe col-sx-12 col-sm-12 col-md-7 col-lg-7">
-            <h1 tabindex="5"><?php echo $title?></h1>
+            <h1 tabindex="7"><?php echo $title?></h1>
             <img alt="Image of <?php echo $title?>" class="recipe-img" src="" style="background-image: url(<?php echo $image?>);">
             <div class="holder">
-	        	<div><span class="glyphicon glyphicon-time"></span> <?php echo $preptime;?> min</div>
-                <div><span class="glyphicon glyphicon-tasks"></span> <?php echo $calories;?> calories</div>
-                <div><span class="glyphicon glyphicon-user"></span> <?php echo $serves;?> servings</div>
+	        	<div><span class="glyphicon glyphicon-time"></span> <span tabindex="7"><?php echo $preptime;?> minutes</span></div>
+                <div><span class="glyphicon glyphicon-tasks"></span> <span tabindex="7"><?php echo $calories;?> calories</span></div>
+                <div><span class="glyphicon glyphicon-user"></span> <span tabindex="7"><?php echo $serves;?> servings</span></div>
 			    <?php if(!empty($diettype)) { ?>
-			    	<div><span class="glyphicon glyphicon-leaf"></span> <?php echo !empty($diettype) ? $diettype : ''; ?> </div>
+			    	<div><span tabindex="7" class="glyphicon glyphicon-leaf"></span> <?php echo !empty($diettype) ? $diettype : ''; ?> </div>
 				<?php } ?>
             </div>
         </div>
@@ -16,7 +16,7 @@
             <h2 tabindex="8">Ingredients:</h2>
             <ul>
                 <?php foreach ($narrative->ingredients as $ingredient):?>
-                <li>
+                <li tabindex="8">
                     <span class="amount"><?php echo $ingredient->quantity.$ingredient->units?></span>
                     <span class="ingredient-name"><?php echo $ingredient->name?></span>
                 </li>
@@ -26,46 +26,46 @@
         <div class="recipe col-sx-12 col-sm-12 col-md-7 col-lg-7">
             <ul class="nav nav-tabs nav-justified">
                 <li class="<?php echo $defaultStyle == 'narrative' ? 'active' : '' ?>">
-					<a class="recipe-style" href="#narrative" data-style="narrative"
+					<a tabindex="9" class="recipe-style" href="#narrative" data-style="narrative"
                        data-toggle="tab" data-target="#narrative">narrative</a>
                 </li>
                 <li class="<?php echo $defaultStyle == 'segmented' ? 'active' : '' ?>">
-					<a class="recipe-style" href="#segmented" data-style="segmented"
+					<a tabindex="9" class="recipe-style" href="#segmented" data-style="segmented"
                        data-toggle="tab" data-target="#segmented">segmented</a>
                 </li>
                 <li class="<?php echo $defaultStyle == 'step' ? 'active' : '' ?>">
-					<a class="recipe-style" href="#step-by-step" data-style="step"
+					<a tabindex="9" class="recipe-style" href="#step-by-step" data-style="step"
                        data-toggle="tab" data-target="#step-by-step">step-by-step</a>
                 </li>
             </ul>
             <div class="tab-content">
-                <div tabindex="7" class="tab-pane fade <?php
+                <div class="tab-pane fade <?php
                     echo $defaultStyle == 'narrative' ? 'in active' : '' ?>" id="narrative">
-                    <h2 tabindex="8">Instructions:</h2>
-                    <section tabindex="9">
+                    <h2 tabindex="10">Instructions:</h2>
+                    <section tabindex="11">
                         <?php echo $narrative->instructions;?>
                     </section>
                 </div>
-                <div tabindex="7" class="tab-pane fade <?php
+                <div class="tab-pane fade <?php
                     echo $defaultStyle == 'step' ? 'in active' : '' ?>" id="step-by-step">
-                    <h2 tabindex="8">Instructions:</h2>
-                    <ol tabindex="9">
+                    <h2 tabindex="10">Instructions:</h2>
+                    <ol>
                         <?php foreach ($steps->instructions as $step):?>
-                        <li><?php echo $step->instruction?></li>
+                        <li tabindex="11"><?php echo $step->instruction?></li>
                         <?php endforeach?>
                     </ol>
                 </div>
-                <div tabindex="7" class="tab-pane fade <?php
+                <div class="tab-pane fade <?php
                     echo $defaultStyle == 'segmented' ? 'in active' : '' ?>" id="segmented">
-                    <h2 tabindex="8">Instructions:</h2>
+                    <h2 tabindex="10">Instructions:</h2>
 
-                    <ul tabindex="9" id='timeline'>
+                    <ul id='timeline'>
                         <?php foreach ($segmented->instructions as $step):?>
-                        <li tabindex="10">
+                        <li tabindex="11">
                             <input class="radio" id="work<?php echo $step->stepid?>" name="works" type="radio"
                                 <?php echo $step->stepid == 1 ? ' checked' : ''?>>
                             <div class="relative">
-                                <label for='work<?php echo $step->stepid?>'>
+                                <label tabindex="11" for='work<?php echo $step->stepid?>'>
                                 <?php echo $step->instruction?>
                                 </label>
                                 <span class='step'><?php echo $step->stepid?></span>
