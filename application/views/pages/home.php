0803e887f15bf5236907932e1f0a8b3b70970697
Aleks <achudars@gmail.com>
2014-03-14 22:57:58 +0000

    Changed home page. validated in W3C.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -11,46 +11,52 @@
 	</div>
 	<div class="container welcome-message">
 		<div tabindex="7" class="alert alert-info">
-			<p>First Things First.</p>
-			<p>Before you go ahead and start cooking, we'd like to know how good you are in the kitchen.</p>
-			<p>This application is tailored to you and your preferences for recipe instructions.</p>
-			<p>Simply pick your preferred style from the options below and you'll be ready to go.</p>
+			<h3>Before selecting <strong>recipes</strong> in the <strong>top left corner</strong> choose your cooking instruction style!</h3>
 		</div>
 	</div>
 	<div class="container">
 		<div class="home-squares col-xs-12 col-sm-4 col-md-4 col-lg-4">
 			<div class="holder">
-				<img alt="Image of Narrative Instructions" src="//:0" class="preview-img" style="background-image: url(<?php echo base_url() . 'assets/images/narrative.png' ?>);">
 				<div tabindex="8" class="course-title">
-					<p >Narrative Instructions</p>
+					<h1>Narrative Instructions</h1>
 				</div>
-				<div tabindex="9" class="style-intro">
+
+				<div class="ribbon-wrapper"><div tabindex="9" class="ribbon">EXPERT</div></div>
+
+				<a tabindex="10" class="btn btn-primary recipe-style home-style-choose" data-style="narrative">Choose Narrative Style</a>
+
+				<div tabindex="11" class="style-intro">
 					<div>The narrative style is for those who don't want to be bounded by culineray rules. Rough steps, rough instructions.</div>
-					<a tabindex="10" class="btn btn-primary recipe-style home-style-choose" data-style="narrative">Choose Narrative Style</a>
 				</div>
 			</div>
 		</div>
 		<div class="home-squares col-xs-12 col-sm-4 col-md-4 col-lg-4">
 			<div class="holder">
-				<img alt="Image of Segmented Instructions" src="//:0" class="preview-img" style="background-image: url(<?php echo base_url() . 'assets/images/segmented.png' ?>);">
-				<div tabindex="11" class="course-title">
-					<p>Segmented Instructions</p>
+				<div tabindex="12" class="course-title">
+					<h1>Segmented Instructions</h1>
 				</div>
-				<div tabindex="12" class="style-intro">
-					<div>The segmented style is well suited to those with intermediate experience in the kitchen, providing the information for the recipe in several, not too complicated steps.</div>
-					<a tabindex="13" class="btn btn-primary recipe-style home-style-choose" data-style="segmented">Choose Segmented Style</a>
-				</div>
-			</div>
-		</div>
-		<div class="home-squares col-xs-12 col-sm-4 col-md-4 col-lg-4">
-			<div class="holder">
-				<img alt="Image of Step-By-Step Instructions" src="//:0" class="preview-img" style="background-image: url(<?php echo base_url() . 'assets/images/steps.png' ?>);">
-				<div tabindex="14" class="course-title">
-					<p>Step-By-Step Instructions</p>
-				</div>
+
+				<div class="ribbon-wrapper"><div tabindex="13" class="ribbon">COMPETENT</div></div>
+
+				<a tabindex="14" class="btn btn-primary recipe-style home-style-choose" data-style="segmented">Choose Segmented Style</a>
+
 				<div tabindex="15" class="style-intro">
+					<div>The segmented style is well suited to those with intermediate experience in the kitchen, providing the information for the recipe in several, not too complicated steps.</div>
+				</div>
+			</div>
+		</div>
+		<div class="home-squares col-xs-12 col-sm-4 col-md-4 col-lg-4">
+			<div class="holder">
+				<div tabindex="16" class="course-title">
+					<h1>Step-By-Step Instructions</h1>
+				</div>
+
+				<div class="ribbon-wrapper"><div tabindex="17" class="ribbon">NOVICE</div></div>
+
+				<a tabindex="18" class="btn btn-primary recipe-style home-style-choose" data-style="step">Choose Step-By-Step Style</a>
+
+				<div tabindex="19" class="style-intro">
 					<div>Not really one for cooking? Not really sure how to sauté a potato? Simple, easy and well defined steps - this is the choice for you.</div>
-					<a tabindex="16" class="btn btn-primary recipe-style home-style-choose" data-style="step">Choose Step-By-Step Style</a>
 				</div>
 			</div>
 		</div>
